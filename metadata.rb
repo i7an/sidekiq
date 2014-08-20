60d24c9f33110ec247155747fae8cd6bc7ca3168
Ivan <i.yurchenko@itransition.com>
2014-08-20 13:26:38 +0300

    metadata

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'sidekiq-recipe'
+maintainer       'Itransition'
+maintainer_email 'i.yurchenko@itransition.com'
+license          'Apache 2.0'
+description      'Configures sidekiq'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1'
+
+supports 'ubuntu'
