28cd941bca58bb5183c156ef51cfa2a1bdf49d71
Ivan <i.yurchenko@itransition.com>
2014-08-20 13:30:49 +0300

    readme path

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Itransition'
 maintainer_email 'i.yurchenko@itransition.com'
 license          'Apache 2.0'
 description      'Configures sidekiq'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          '0.1'
 
 supports 'ubuntu'
